8be9b50086e7360c10ee8225fc1de6ffd3109243
mout12 <mout12@gmail.com>
2018-03-11 15:10:11 -0700

    Howto: C# date string to JS date type

--- MVC5App/Scripts/Test/JacksonScript.js
+++ MVC5App/Scripts/Test/JacksonScript.js
@@ -4,18 +4,9 @@ function PrintHello() {
     document.write(message);
 }
 
-function PrintDate() {
-    var dateStr = "2017 - 07 - 09 11:07:00 UTC";
-    var dateArr = dateStr.split(" ");
+function PrintDateFromStr(inputStr) {
+    var dateArr = inputStr.split(" ");
     var fullDate = new Date(dateArr[0], dateArr[2] + 1, dateArr[4]);
 
-    document.write("<p>", fullDate, "</p>");
-
-    document.write("Year: ", dateArr[0], "<br />");
-    document.write("Month: ", dateArr[2], "<br />");
-    document.write("Day: ", dateArr[4], "<br />");    
-}
-
-function PrintDateFromStr(inputStr) {
-    document.write(inputStr);
+    document.write(fullDate);
 }
