9d164997131e1121021a1b874012acbf5c03c043
mout12 <mout12@gmail.com>
2018-03-11 14:07:33 -0700

    JS: howto parse CSV date to JS format for charts.

--- MVC5App/Scripts/Test/JacksonScript.js
+++ MVC5App/Scripts/Test/JacksonScript.js
@@ -3,3 +3,15 @@ function PrintHello() {
     var message = "hello world?";
     document.write(message);
 }
+
+function PrintDate() {
+    var dateStr = "2017 - 07 - 09 11:07:00 UTC";
+    var dateArr = dateStr.split(" ");
+    var fullDate = new Date(dateArr[0], dateArr[2] + 1, dateArr[4]);
+
+    document.write("<p>", fullDate, "</p>");
+
+    document.write("Year: ", dateArr[0], "<br />");
+    document.write("Month: ", dateArr[2], "<br />");
+    document.write("Day: ", dateArr[4], "<br />");    
+}
