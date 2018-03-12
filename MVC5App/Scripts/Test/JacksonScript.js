bcb3079ba0dafc61d91bcae48dd041d5355dad84
mout12 <mout12@gmail.com>
2018-03-11 17:28:07 -0700

    Chart displays dates generated in C#

--- MVC5App/Scripts/Test/JacksonScript.js
+++ MVC5App/Scripts/Test/JacksonScript.js
@@ -11,3 +11,14 @@ function PrintDateFromStr(inputStr) {
     fullDate.setFullYear(dateArr[0], dateArr[2] - 1, dateArr[4]);
     document.write(fullDate, "<br />");
 }
+
+
+function ParseDate(inputStr) {
+    var dateArr = inputStr.split(" ");
+    var fullDate = new Date();
+
+    fullDate.setFullYear(dateArr[0], dateArr[2] - 1, dateArr[4]);
+    document.write(fullDate, "<br />");
+
+    return fullDate;
+}
